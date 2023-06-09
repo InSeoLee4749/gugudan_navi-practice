d52890b467d4a565f413bb691361fd4565a64139
LeeInSeo <in474900@gmail.com>
2023-06-09 17:22:31 +0900

    F&Q JS 구동 연습

--- js/jquery.js
+++ js/jquery.js
@@ -19,8 +19,24 @@ $(document).ready(function(){
         $(".bi-bootstrap-fill").removeClass("text-info")
         $(".bi-bootstrap-fill").eq(count).addClass('text-info')
     }
-})
-
 // setinterval 은 바로 터지는 것이 아니다. 정해진 시간 이후부터 
 // settimeout(실행함수, 시간) -> 시간이 지나면 한번 실행하라
+
+    //사전인터뷰 시간차를 두고 오픈하기
+    // $(".accordion-item .accordion-button").addClass("collapsed")  //초기에 모든 아코디언버튼이 닫혀있는 상태
+    
+    let faqnum = 0;
+    setInterval(faqopen, 3000)
+    function faqopen(){
+        faqnum++;
+        faqnum %=5;
+
+        $(".accordion-item .accordion-collapse").removeClass("show")  //show를 제거, 아코디언 콘텐츠를 닫는다
+        $(".accordion-item").eq(faqnum).find(".accordion-collapse").addClass("show") //show를 추가, 아코디언 연다
+
+        $(".accordion-item .accordion-button").addClass("collapsed") //아코디언 버튼을 닫힌 상태
+        $(".accordion-item").eq(faqnum).find(".accordion-button").removeClass("collapsed") // 아코디언 버튼 열린 상태
+    }
+})
+
 
--- js/node.js
+++ js/node.js
@@ -1,17 +1,53 @@
-let num = 0;
-const nm = ["Nodejs","react","jquery","figma","plan"]
+// let num = 0;
+// const nm = ["Nodejs","react","jquery","figma","plan"]
 
-setInterval(
-    addnum,
-    1000
-)
+// setInterval(
+//     addnum,
+//     1000
+// )
 
-function addnum(){
+// function addnum(){
     
-    console.log( nm[num] + "훈련중") //nm[nm]은 nm이 순서대로 반복
-    num++; //1씩 늘려라
-    num = num % nm.length;  //5를 5로 나누니깐 nm에서 0.1.2.3.4 만 반복
+//     console.log( nm[num] + "훈련중") //nm[nm]은 nm이 순서대로 반복
+//     num++; //1씩 늘려라
+//     num = num % nm.length;  //5를 5로 나누니깐 nm에서 0.1.2.3.4 만 반복
 
+// }
+// 1Nodejs훈련중 2react훈련중 3jquery훈련중... 라고 나온다.
+
+// let count = 0;
+// function gugudan(){
+//     count += 2;
+//     console.log(count);
+// }
+// setInterval(gugudan, 3000)
+
+const navidata = [
+    {
+        d1 : "네이버",
+        d1link : "http://www.naver.com",
+        cls : ["d1","px-5","text-dark"]
+    },
+    {
+        d1 : "다음",
+        d1link : "http://www.daum.com",
+        cls : ["d1","px-1",""]
+    },
+    {
+        d1 : "구글",
+        d1link : "http://www.google.com",
+        cls : ["google","px-2","text-dark"]
+    }
+]
+
+// console.log(navidata[1].d1);  //다음 출력됨
+
+// console.log(navidata[2].cls[0]); //google 출력됨
+
+// <a href=http://www.daum.net>다음</a> 와 같이 출력되도록 하라(EC6로 표현)
+// 과정 1 -> console.log('<a href=http://www.daum.net>${navidata[1].d1}</a>')
+// 결과값 -> console.log('<a href=${navidata[1].d1link}>${navidata[1].d1}</a>')
+
+for(x in navidata){
+    console.log(`<a href=${navidata[x].d1link}>${navidata[x].d1}</a>`)
 }
-
-// 1Nodejs훈련중 2react훈련중 3jquery훈련중... 라고 나온다
